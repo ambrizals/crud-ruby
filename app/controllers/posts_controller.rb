de0fbbf075a4187e74ac814b9a146f399f80e37c
Ambrizal Suryadinata <sabuncolek@ambrizal.net>
2018-07-10 22:47:19 +0800

    Update SQLite3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :restore]
 
   # GET /posts
   # GET /posts.json
@@ -70,7 +70,7 @@ class PostsController < ApplicationController
     @post.is_delete = false
     @post.save
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully restored.' }
+      format.html { redirect_to posts_delete_url, notice: 'Post was successfully restored.' }
       format.json { head :no_content }
     end
   end
