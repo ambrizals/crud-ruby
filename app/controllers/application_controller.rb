858dc6d3c39b2f5f2229c6e50ad9bc0c67e2137a
ambrizals <ambrizals@outlook.co.id>
2018-07-20 16:58:56 +0800

    Complete CRUD With Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :configure_sign_up_params, if: :devise_controller?
+
+    protected
+    def configure_sign_up_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nama, :phone, :username])
+    end
 end
